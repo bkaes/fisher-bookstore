39696d3ce71cefb6b68d0c3242bfcf034b703838
Bkaes <bkaes8@gmail.com>
2020-02-16 18:01:31 -0500

    Books-Feature Lab3 Piece Complete

---
+++ src/books/Books.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Books.css";
+
+export default function Home() {
+    return (
+        <div className="Home">
+            <div className="lander">
+                <h1>Books</h1>
+                <p>Here be Books</p>
+            </div>
+        </div>
+    );
+}
