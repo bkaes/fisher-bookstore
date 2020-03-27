2b2182c78087ede864d9ec8660eab399fb849d47
Bkaes <bkaes8@gmail.com>
2020-03-27 16:02:58 -0400

    api implementation

--- src/authors/Authors.js
+++ src/authors/Authors.js
@@ -1,59 +1,21 @@
-import React, {Component} from "react";
+import React, { useEffect, useState} from "react";
 import "./Authors.css";
 import { AuthorDisplay } from "./AuthorDisplay";
 
-export default class Authors extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            authors: [                
-                {
-                id: 1,
-                name: "Eric Evans",
-                description: "an Author ",
-                bookNum: 7
-            },
-            {
-                id: 2,
-                name: "Josh Jacobs",
-                description: "a Running back ",
-                bookNum: 0
-            },
-            {
-                id: 3,
-                name: "Patrick Mahomes",
-                description: "a Quarter back ",
-                bookNum: 0
-            },
-            {
-                id: 4,
-                name: "Eric Evans",
-                description: "an Author ",
-                bookNum: 7
-            },
-            {
-                id: 2,
-                name: "Josh Jacobs",
-                description: "a Running back ",
-                bookNum: 0
-            },
-            {
-                id: 3,
-                name: "Patrick Mahomes",
-                description: "a Quarter back ",
-                bookNum: 0
-            },
-        ]
-        };
-    }
-    render() {
-    return (
-        <div className="Author">
-            <div className="lander">
-                <h1>Authors</h1>
-                <AuthorDisplay authors={this.state.authors} />
-            </div>
+export default function Authors (props) 
+{
+    const [data, setData] = useState([]);
 
+    useEffect(()=> {
+        fetch("https://localhost:5001/api/authors/")
+            .then(response => response.json())
+            .then(data => setData(data));
+    }, []);
+    return (
+        <div className="Authors">
+            <div className="lander">
+                <AuthorDisplay authors={data} />
+            </div>
         </div>
     );
-}}
+}
